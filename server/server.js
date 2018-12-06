b8f5d8e8d516adf3f96320b391a01df50c73e510
Nitin SINGH <nitinkumar.singh@f5.com>
2018-12-07 00:11:48 +0530

    init commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express')
+
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+// app.get('/', (req,res)=>{
+//     res.sendFile(path.join(publicPath, 'index.html'));
+// })
+
+
+app.listen(port, ()=>{
+    console.log('chat app server starting on port', port);
+})
+
+
+
+
+// console.log(__dirname + '\..\\public');
+// console.log(publicPath);
+
