2b61483cf3c783b737dbf73a6fa7b0ff50e0ca4f
Nitin SINGH <nitinkumar.singh@f5.com>
2018-12-07 14:52:57 +0530

    adding socket.io to an app

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express')
-
+const http = require('http');
+const socketIO = require('socket.io');
 const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
@@ -10,9 +11,21 @@ app.use(express.static(publicPath));
 // app.get('/', (req,res)=>{
 //     res.sendFile(path.join(publicPath, 'index.html'));
 // })
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+
+    socket.on('disconnect', ()=>{
+        console.log('Disconnect from client');
+    })
+})
 
 
-app.listen(port, ()=>{
+
+server.listen(port, ()=>{
     console.log('chat app server starting on port', port);
 })
 
