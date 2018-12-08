5e1718ac9639e2453bf8cf5e5cc0ddafa3c1107d
Nitin SINGH <nitinkumar.singh@f5.com>
2018-12-08 17:53:04 +0530

    Add form for messages & show incoming msg in browser

--- public/js/index.js
+++ public/js/index.js
@@ -17,14 +17,37 @@ socket.on('disconnect', function () {
 //     text: 'Hey this is nitin!!!'
 // });
 
+// socket.on('newMessage', function (newMsg) {
+//     console.log('New Message: ', newMsg);
+// });
+
+// socket.emit('createMessage', {
+//     from: 'sachin.kumar@exaple.com',
+//     text: 'This is going to be my first message create in chat app.'
+// }, function callback(message) {
+//     console.log('Got it', message);
+// });
+
+// introducing jquery 
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    var msg = jQuery('[name=message-input]').val();
+
+    socket.emit('createMessage', {
+        from: 'Some User',
+        text: msg,
+    }, function callback(message) {
+        console.log('Got it', message);
+    });
+    
+});
+
 socket.on('newMessage', function (newMsg) {
     console.log('New Message: ', newMsg);
-});
+    var li = jQuery('<li></li>');
+    li.text(`${newMsg.from} : ${newMsg.text}`);
 
-socket.emit('createMessage', {
-    from: 'sachin.kumar@exaple.com',
-    text: 'This is going to be my first message create in chat app.'
-}, function callback(message){
-    console.log('Got it', message);
+    jQuery('#messages').append(li);
 });
-
